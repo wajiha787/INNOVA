18aa67618a3aa64ce9421fb7d8becd2fdfd96c7f
wajiha787 <wajihawaqar1234@gmail.com>
2025-04-02 14:59:32 +0500

    Header Done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
+import Home from "./Components/Home";
 import Explore from "./Pages/Explore";
 import Create from "./Pages/Create";
 import Contact from "./Pages/Contact";
@@ -9,6 +10,7 @@ function App() {
   return (
     <Router>
       <Header />
+      <Home />
       <Routes>
         <Route path="/" element={<Explore />} />
         <Route path="/explore" element={<Explore />} />
